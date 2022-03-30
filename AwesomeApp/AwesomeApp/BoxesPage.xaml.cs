a01fba95f067c467d92ea25c2bde546c6ec2bf34
OldestSalt <super.senya2002@yandex.ru>
2022-03-30 23:48:49 +1000

    More boxes!!!

---
+++ AwesomeApp/AwesomeApp/BoxesPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AwesomeApp {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoxesPage : ContentPage {
+        public BoxesPage() {
+            InitializeComponent();
+            BindableLayout.SetItemsSource(boxes, MainPage.Boxes);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e) {
+            MainPage.Boxes.Add(new MyBox());
+        }
+
+        private void resetBox(object sender, EventArgs e) {
+            var box = MainPage.Boxes.Where(i => i.id == Int32.Parse((sender as Button).ClassId)).FirstOrDefault();
+            box.xTranslate = 0.0;
+            box.yTranslate = 0.0;
+            box.rot = 0.0;
+        }
+
+        private void removeBox(object sender, EventArgs e) {
+            MainPage.Boxes.Remove(MainPage.Boxes.Where(i => i.id == Int32.Parse((sender as Button).ClassId)).FirstOrDefault());
+        }
+    }
+}
--- AwesomeApp/AwesomeApp/MainPage.xaml.cs
+++ AwesomeApp/AwesomeApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ using Xamarin.Forms;
 
 namespace AwesomeApp {
     public partial class MainPage : ContentPage {
-        private Dictionary<string, Color> pickerColors = new Dictionary<string, Color> {
+        private static Dictionary<string, Color> pickerColors = new Dictionary<string, Color> {
             {"White", Color.White},
             {"Blue", Color.Blue},
             {"Red", Color.Red},
@@ -16,37 +17,147 @@ namespace AwesomeApp {
             {"Green", Color.Green},
             {"Orange", Color.Orange}
         };
+
+        public static ObservableCollection<MyBox> Boxes = new ObservableCollection<MyBox>();
+
+        private Frame selectedBox;
+        private MyBox selectedBoxObj;
         public MainPage() {
             InitializeComponent();
-
             colors.ItemsSource = pickerColors.Keys.ToList();
             colors.SelectedIndex = 0;
         }
 
         private void userText_TextChanged(object sender, TextChangedEventArgs e) {
-            text.Text = (sender as Entry).Text;
+            if (selectedBox != null) {
+                (selectedBox.Content as Label).Text = (sender as Entry).Text;
+                selectedBoxObj.text = (sender as Entry).Text;
+            }
         }
 
         private void colors_SelectedIndexChanged(object sender, EventArgs e) {
-            box.BackgroundColor = pickerColors[colors.SelectedItem.ToString()];
+            if (selectedBox != null) {
+                selectedBox.BackgroundColor = pickerColors[colors.SelectedItem.ToString()];
+                selectedBoxObj.color = pickerColors[colors.SelectedItem.ToString()];
+            }
         }
 
         private void StepperLeftAndRight_ValueChanged(object sender, ValueChangedEventArgs e) {
-            box.TranslationX = (sender as Stepper).Value;
+            if (selectedBox != null) {
+                selectedBox.TranslationX = (sender as Stepper).Value;
+                selectedBoxObj.xTranslate = (sender as Stepper).Value;
+            }
         }
 
         private void StepperUpAndDown_ValueChanged(object sender, ValueChangedEventArgs e) {
-            box.TranslationY = (sender as Stepper).Value;
+            if (selectedBox != null) {
+                selectedBox.TranslationY = (sender as Stepper).Value;
+                selectedBoxObj.yTranslate = (sender as Stepper).Value;
+            }
         }
 
         private async void Button_Clicked(object sender, EventArgs e) {
             LAR.Value = 0;
             UAD.Value = 0;
-            await DisplayAlert("Successfully!", "Your box has been reset, idiot.", "Ok...");
+            rotation.Value = 0;
+            await DisplayAlert("Successfully!", "Chosen box has been reset, idiot.", "Ok...");
         }
 
-        private async void Button_Clicked_1(object sender, EventArgs e) {
-            await DisplayAlert("Hey, Paha!", "( ° ͜ʖ͡°)╭∩╮\nPal'chik", "Ok...");
+        private void Button_Clicked_1(object sender, EventArgs e) {
+            //await DisplayAlert("Hey, Paha!", "( ° ͜ʖ͡°)╭∩╮\nPal'chik", "Ok...");
+            var boxesPage = new BoxesPage();
+            boxesPage.Disappearing += (biba, boba) => {
+                field.Children.Clear();
+                foreach (var b in Boxes) {
+                    Frame frm = new Frame() {
+                        BackgroundColor = b.color,
+                        ClassId = b.id.ToString(),
+                        TranslationX = b.xTranslate,
+                        TranslationY = b.yTranslate,
+                        Rotation = b.rot
+                    };
+                    frm.Content = new Label() {
+                        Text = b.text,
+                        TextColor = Color.Black,
+                        HorizontalOptions = LayoutOptions.Center,
+                        VerticalOptions = LayoutOptions.Center,
+                        HorizontalTextAlignment = TextAlignment.Center
+                    };
+
+                    var ev = new TapGestureRecognizer();
+                    ev.Tapped += (aboba, abeba) => {
+                        if (selectedBox != null) {
+                            selectedBox.BorderColor = Color.Transparent;
+                        }
+                        selectedBox = frm;
+                        selectedBoxObj = Boxes.Where(i => i.id == Int32.Parse(frm.ClassId)).FirstOrDefault();
+                        frm.BorderColor = Color.Red;
+
+                        colors.SelectedItem = pickerColors.Where(i => i.Value == frm.BackgroundColor).FirstOrDefault().Key;
+                        entry.Text = (frm.Content as Label).Text;
+                        LAR.Value = frm.TranslationX;
+                        UAD.Value = frm.TranslationY;
+                        rotation.Value = frm.Rotation;
+                    };
+                    ev.NumberOfTapsRequired = 1;
+                    frm.GestureRecognizers.Add(ev);
+
+                    field.Children.Add(frm,
+                        Constraint.RelativeToParent(p => {
+                            return p.Width * 0.5 - 75;
+                        }),
+                        Constraint.RelativeToParent(p => {
+                            return p.Height * 0.5 - 75;
+                        }),
+                        Constraint.Constant(150),
+                        Constraint.Constant(150)
+                    );
+                }
+            };
+
+            Navigation.PushAsync(boxesPage);
+
+        }
+
+        private void rotation_ValueChanged(object sender, ValueChangedEventArgs e) {
+            selectedBox.Rotation = (sender as Slider).Value;
+            selectedBoxObj.rot = (sender as Slider).Value;
+        }
+
+        private void boxChosen(object sender, EventArgs e) {
+            selectedBox = sender as Frame;
+        }
+    }
+
+    public class MyBox {
+        private static int counter = 0;
+        public Color color {
+            get; set;
+        } = Color.White;
+
+        public string text {
+            get; set;
+        } = "Hello world!";
+
+        public double xTranslate {
+            get; set;
+        } = 0;
+
+        public double yTranslate {
+            get; set;
+        } = 0;
+
+        public double rot {
+            get; set;
+        } = 0.0;
+
+        public int id {
+            get;
+        }
+
+        public MyBox() {
+            ++counter;
+            id = counter;
         }
     }
 }
